80b5cf2683dae9f125430668f9b6dcd2fe1445be
Lauren McCarey <lrm2@williams.edu>
2022-04-29 14:27:14 -0400

    kinda working

--- practice/Library.fs
+++ practice/Library.fs
@@ -13,8 +13,9 @@ let pcircle = pstr "circle" |>> (fun _ -> Circle)
 let pcircles = pmany1 pcircle
 let grammar = pleft pcircles peof
 
-let rec eval(e: Expr) (x: int) : string =
+let rec eval(e: Expr) (cx: int) (cy: int) (r: int): string =
     match e with 
     | Shapes [] -> ""
     | Shapes (s::ss) -> "\t<circle cx='" + (string x) + "' cy='100' r='80' fill='green' />\n" + (eval (Shapes ss) (x + 100)) 
+    // | Shapes (s::ss) -> "\t<circle cx='" + (string cx) + "' cy='" + (string cy) + "' r='" + (string r) + "' fill='none' stroke='black' />\n" + (eval (Shapes ss) cx cy (r / 2)) 
     
