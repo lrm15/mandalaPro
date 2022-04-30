4ddf64d7c6a18daa453a2c919025b22c42f91dfc
Lauren McCarey <lrm2@williams.edu>
2022-04-29 21:13:50 -0400

    experimenting with mandala size

--- practice/Program.fs
+++ practice/Program.fs
@@ -6,10 +6,10 @@ let main argv =
 
     use sw = new StreamWriter("newpractice.svg")
 
-    sw.WriteLine("<svg version='1.1'\n\t width='300' height='200'\n\t xmlns='http://www.w3.org/2000/svg'>")
+    sw.WriteLine("<svg version='1.1'\n\t width='500' height='500'\n\t xmlns='http://www.w3.org/2000/svg'>")
 
     // sw.WriteLine("<svg>") // start tag
-    sw.WriteLine(eval (Shapes [Circle; Circle; Circle; Rect]) 100 100 80) 
+    sw.WriteLine(eval (Shapes [Circle; Circle; Circle; Rect]) 250 250 200) 
     sw.WriteLine("</svg>") // end tag
 
     0
