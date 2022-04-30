77588b15cf7d68423a07b6a39c401670d4a89aa3
Lauren McCarey <lrm2@williams.edu>
2022-04-29 21:03:44 -0400

    trying to build a parser

--- lang/ProjectParser.fs
+++ lang/ProjectParser.fs
@@ -1,1 +1,130 @@
 module ProjectParser 
+
+open Parser 
+
+(* A primitive type representing permissible colors *)
+type Color = 
+| Blue 
+// | Grey 
+// | Indigo 
+// | Green
+// | Turquoise
+// | Blueviolet
+// | Red
+// | White
+// | Pink
+// | Purple
+// | Yellow 
+// | Black 
+
+(* A primitive type representing the size of a shape *)
+type Size = 
+| Num of int  
+
+(* A primitive type representing permissible shapes *)
+type Shape = 
+| Circle of Color * Size
+| Square of Color * Size 
+
+(* A combining form representing a mandala *)
+type Expr = 
+| Mandala of Shape list  
+
+let pexpr, pexprImpl = recparser()
+
+(*
+ * Helper method to parse the color blue. 
+ *
+ * @param 
+ * @return   
+ *)
+let pblue = pstr "blue" |>> (fun _ -> Blue)
+
+(* 
+ * Helper method to parse any permissible color. 
+ * 
+ * @param 
+ * @return 
+ *)
+ let pcolor = pblue // <|> etc
+
+(*
+ * Helper method to parse the fill color for a shape. 
+ *  
+ * @param 
+ * @return 
+ *)
+let pfillcolor =
+    pbetween 
+        (pstr "fill_color = '")
+        (pchar "';")
+        pcolor 
+
+(*
+ * Helper method to parse the stroke color for a shape. 
+ *  
+ * @param 
+ * @return 
+ *)
+let pstrokecolor =
+    pbetween 
+        (pstr "stroke_color = '")
+        (pstr "';")
+        pcolor 
+
+(*
+ * Helper method to parse the size information for a shape. 
+ * 
+ * @param 
+ * @return 
+ *)
+let psize = 
+    pbetween 
+        (pstr "size = ")
+        (pchar ';')
+        pdigit 
+
+(*
+ * Helper method to parse the color and size information for a shape. 
+ * ex: Circle (fill_color = 'blue'; stroke_color = 'black'; size = 100;)
+ * 
+ * @param 
+ * @return  
+ *)
+let pspecs = 
+    pseq 
+        pfillcolor
+        pseq 
+            pstrokecolor
+            psize 
+            (fun something ?)
+        (want to return specs in a form that can be passed to a shape constructor)
+
+
+(* 
+ * Helper method to parse a circle and its information. 
+ *
+ * @param 
+ * @return 
+ *)
+let pcircle = 
+    pbetween 
+        (pstr "Circle (")
+        (pchar ')')
+        pspecs |>> Circle (x, y, z)
+
+(* 
+ * Helper method to parse a shape. 
+ * 
+ * @param 
+ * @return 
+ *)
+let pshape = pcircle // <|> psquare ...
+
+// let pgrey = pstr "grey" |>> (fun _ -> Grey)
+let pcircle = pstr "circle" |>> (fun _ -> Circle)
+// let psquare = pstr "square" |>> (fun _ -> Square)
+
+
+
+
